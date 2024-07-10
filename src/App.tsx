891cf08e9b5f83341c2d98b415ac8806864e67f9
Dan Hems <danhems87@gmail.com>
2024-07-10 20:00:16 +0100

    Use some Grafana components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from 'react';
+import { Field, Input, Card } from "@grafana/ui";
 import './App.css';
 
 export type item = {
@@ -35,12 +36,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <input id="search-field" type="text" placeholder="Enter search term" onChange={onInputChange} />
-        <ul>
+        <Field label="Enter a search term" description="Your search term will be matched against each item's title text">
+          <Input id="search-field" onChange={onInputChange} />
+        </Field>
+        <ul style={{
+          padding: '20px',
+          listStyle: 'none',
+          display: 'grid'
+        }}>
           {searchResult.map((item: any, index: number) => (
             <li key={"item-" + index}>
-              <h2>{item.title}</h2>
-              <p>{item.body}</p>
+              <Card>
+                <Card.Heading>{item.title}</Card.Heading>
+                <Card.Description>{item.body}</Card.Description>
+              </Card>
             </li>
           ))}
         </ul>
